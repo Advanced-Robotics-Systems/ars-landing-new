6a302730e9d0fd1aad70aeee4852cb9cc9c0a896
A1-mamun <1154almamun@gmail.com>
2024-12-12 02:19:51 +0600

    Icon Navlink, footer link and footer social link data added

---
+++ src/app/data/index.ts
@@ -0,0 +1,81 @@
+import { ICONS } from "../utils/icons";
+
+// Navbar Links
+export const navLinks = [
+  {
+    name: "Products",
+    link: "/products",
+  },
+  {
+    name: "Company",
+    link: "/company",
+  },
+
+  {
+    name: "Careers",
+    link: "/careers",
+  },
+
+  {
+    name: "Get in Touch",
+    link: "/get-in-touch",
+  },
+];
+
+// Footer Links
+export const footerLinks = {
+  navigation: [
+    {
+      name: "Products",
+      link: "/products",
+    },
+    {
+      name: "Aboute",
+      link: "/about",
+    },
+    {
+      name: "Careers",
+      link: "/careers",
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+    },
+  ],
+  "helpful-links": [
+    {
+      name: "FAQ",
+      link: "/faq",
+    },
+    {
+      name: "Support",
+      link: "/support",
+    },
+  ],
+  resources: [
+    {
+      name: "Case Studies",
+      link: "/case-studies",
+    },
+    {
+      name: "Blogs",
+      link: "/blogs",
+    },
+  ],
+};
+
+// Footer Socials
+export const footerSocials = [
+  {
+    icon: ICONS.instagram,
+    link: "https://www.instagram.com/arsystemsofficial/",
+  },
+  {
+    icon: ICONS.facebook,
+    link: "https://www.facebook.com/profile.php?id=61562469073840",
+  },
+  {
+    icon: ICONS.linkedin,
+    link: "https://www.linkedin.com/company/advanced-robotics-systems/",
+  },
+];
---
+++ src/app/utils/icons/index.tsx
@@ -0,0 +1,7 @@
+import { FaInstagram, FaLinkedin, FaSquareFacebook } from "react-icons/fa6";
+
+export const ICONS = {
+  instagram: <FaInstagram />,
+  linkedin: <FaLinkedin />,
+  facebook: <FaSquareFacebook />,
+};
