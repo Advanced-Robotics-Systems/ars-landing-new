c9fa09f97a067a1806c660876baf65e015260e50
A1-mamun <1154almamun@gmail.com>
2024-12-19 01:58:40 +0600

    Mission section bg changed

--- src/sections/home/mission.tsx
+++ src/sections/home/mission.tsx
@@ -15,8 +15,8 @@ const OurMission = () => {
         backgroundPosition: "center",
       }}
     >
-      <div className="bg-gradient-to-t from-[#FFFFFFC8] via-[#0590C866] to-ars-cyan-60 h-full text-white">
-        <div className="h-full bg-gradient-to-tl from-[#FFFFFF00] to-[#000000CC] padding-responsive flex flex-col justify-center gap-1 md:gap-3 lg:gap-5">
+      <div className="bg-gradient-to-t from-ice-blue via-[#0590C866] to-ars-cyan-60 h-full text-white">
+        <div className="h-full bg-gradient-to-t from-[#FFFFFF00] to-[#000000CC] padding-responsive flex flex-col gap-1 md:gap-3 lg:gap-4">
           <h5 className="text-sm md:text-xl lg:text-2xl">Our Mission</h5>
           <h2 className="text-lg md:text-3xl lg:text-4xl xl:text-5xl font-medium">
             Revolutionizing Industries with <br /> Cutting-Edge Robotics for an
@@ -29,8 +29,8 @@ const OurMission = () => {
             Automation. Using our in-house robotics and automation system, we
             target to streamline the factory floor to IR 4.0 category.
           </p>
-          <div className="flex gap-2 items-center mt-4 md:mt-8 lg:mt-10">
-            <p className="font-medium">Learn About Us</p>
+          <div className="flex gap-2 items-center mt-4 md:mt-6 lg:mt-8">
+            <p className="font-semibold">Learn About Us</p>
             <Button
               isIconOnly
               radius="full"
