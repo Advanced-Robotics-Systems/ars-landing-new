732b71dd1899bc3ee4229d1230e1a5ddcfb7c424
Abrar Hussain Marzuk <abrar.16@protonmail.com>
2024-12-25 14:12:58 +0600

    Reduced title size in about-backing.tsx

--- src/sections/about/about-backing.tsx
+++ src/sections/about/about-backing.tsx
@@ -6,7 +6,7 @@ const AboutBacking = () => {
   return (
     <section className="bg-ice-blue padding">
       <div className="flex flex-col items-center gap-y-10">
-        <h1 className="text-4xl sm:text-5xl lg:text-6xl text-ars-cyan font-medium">
+        <h1 className="text-3xl xxs:text-4xl sm:text-5xl lg:text-6xl text-ars-cyan text-center font-medium">
           BACKED UP BY
         </h1>
         <div className="flex w-full xxs:w-4/5 md:w-3/5 xl:w-2/5 max-w-[600px] justify-between">
