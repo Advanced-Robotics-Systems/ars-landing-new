23320e156c78be9f10d1fda51b41bbd4f5cabb00
Abrar Hussain Marzuk <abrar.16@protonmail.com>
2024-12-29 14:34:19 +0600

    Fixed Get In Touch alignment

--- src/sections/shared/get-in-touch.tsx
+++ src/sections/shared/get-in-touch.tsx
@@ -5,10 +5,10 @@ const GetInTouch = () => {
   return (
     <section className="flex flex-col sm:flex-row items-center xs:justify-between max-sm:gap-y-4 bg-[#030D2D] padding md:px-24 2xl:px-[12%] 3xl:px-[20%] 4xl:px-[25%]">
       <div className="flex flex-col sm:gap-y-2">
-        <h1 className="text-4xl xxs:text-5xl sm:text-6xl lg:text-7xl text-cyan-blue text-center">
+        <h1 className="text-4xl xxs:text-5xl sm:text-6xl lg:text-7xl text-cyan-blue max-sm:text-center">
           Get In Touch
         </h1>
-        <p className="text-xs sm:text-sm lg:text-base text-white text-center">
+        <p className="text-xs sm:text-sm lg:text-base text-white max-sm:text-center">
           Have questions about our Automations?
         </p>
       </div>
