bde7d0813ac43dc72b56ae72af9326d72aa2578b
Abrar Hussain Marzuk <142195422+abrar-16m@users.noreply.github.com>
2025-01-07 20:48:20 +0600

    Added Navbar in Blogs

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -3,20 +3,23 @@ import React from "react";
 
 const Blogs = () => {
   return (
-    <main
-      className="bg-fixed min-h-screen "
-      style={{
-        background: `
-        linear-gradient(30deg, rgba(5, 144, 200, 0.02) 51.31%, #0D242673 114.45%), 
-        linear-gradient(30deg, #D7EFF9 51.31%, #0590C8B3 114.45%)`,
-      }}
-    >
-      <div className=" h-full">
-        <BlogsHero />
-        <Resources />
-        <GetInTouch />
-      </div>
-    </main>
+    <>
+      <Header />
+      <main
+        className="bg-fixed min-h-screen "
+        style={{
+          background: `
+          linear-gradient(30deg, rgba(5, 144, 200, 0.02) 51.31%, #0D242673 114.45%), 
+          linear-gradient(30deg, #D7EFF9 51.31%, #0590C8B3 114.45%)`,
+        }}
+      >
+        <div className=" h-full">
+          <BlogsHero />
+          <Resources />
+          <GetInTouch />
+        </div>
+      </main>
+    </>
   );
 };
 
