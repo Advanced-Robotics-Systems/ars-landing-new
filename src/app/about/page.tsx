f41c59275e8f9de22891f2258911402b45b08416
A1-mamun <1154almamun@gmail.com>
2025-01-13 12:14:17 +0600

    Moved policy folder into secton folder

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,25 +1,25 @@
 import {
-	AboutBacking,
-	AboutHero,
-	AboutInfo,
-	AboutValues,
-	GetInTouch,
-	Header,
-	JoinOurTeam,
+  AboutBacking,
+  AboutHero,
+  AboutInfo,
+  AboutValues,
+  GetInTouch,
+  Header,
+  JoinOurTeam,
 } from "@/sections";
 
 export default function About() {
-	return (
-		<>
-			<Header />
-			<main>
-				<AboutHero />
-				<AboutInfo />
-				<AboutValues />
-				<AboutBacking />
-				<JoinOurTeam showButton />
-				<GetInTouch />
-			</main>
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <main>
+        <AboutHero />
+        <AboutInfo />
+        <AboutValues />
+        <AboutBacking />
+        <JoinOurTeam showButton />
+        <GetInTouch />
+      </main>
+    </>
+  );
 }
