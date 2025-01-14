afa66160503ef64b890d1b28c7ba19dcdef73334
Abrar Hussain Marzuk <abrar.16@protonmail.com>
2025-01-14 23:18:28 +0600

    Updates made in Products page

--- src/sections/products/hero.tsx
+++ src/sections/products/hero.tsx
@@ -1,6 +1,7 @@
 "use client";
 import heroImage from "@../../../public/A300 Blink GIF.gif";
-import { Button, Image } from "@nextui-org/react";
+import Image from "next/image";
+import { Button } from "@nextui-org/react";
 import { ICONS } from "@/utils/icons";
 import { motion } from "framer-motion";
 import Link from "next/link";
@@ -9,15 +10,15 @@ const ProductsHero = () => {
   return (
     <section className="px-5 md:px-10 lg:px-16 xl:px-40 2xl:px-64 3xl:px-96 py-16 md:py-20 lg:py-20">
       <div className="h-full flex flex-col md:flex-row items-center justify-center rounded-2xl mt-10 lg:mt-7">
-        <div className="md:w-3/5 md:h-[470px] lg:h-[510px] xl:h-[550px] lg:w-2/3 bg-white flex items-center justify-center rounded-t-2xl md:rounded-r-none md:rounded-l-3xl">
+        <div className="md:w-3/5 md:h-[470px] lg:h-[510px] xl:h-[550px] bg-white flex items-center justify-center rounded-t-2xl md:rounded-r-none md:rounded-l-3xl">
           <Image
-            className="lg:-mb-14 rounded-t-2xl md:rounded-l-3xl h-full w-full"
-            src={heroImage.src}
+            className="lg:-mb-14 rounded-t-2xl md:rounded-l-3xl h-full w-full object-contain"
+            src={heroImage}
             alt="ARS A300"
           />
         </div>
         <motion.div
-          className="bg-[#F8F9FC80] text-midnight-blue md:w-2/5 lg:w-1/3 md:h-[470px] lg:h-[510px] xl:h-[550px] rounded-b-2xl md:rounded-l-none md:rounded-r-3xl p-5 lg:p-8 space-y-3 md:space-y-5 lg:space-y-8 xl:space-y-10 flex flex-col justify-center"
+          className="bg-[#F8F9FC80] text-midnight-blue md:w-2/5 md:h-[470px] lg:h-[510px] xl:h-[550px] rounded-b-2xl md:rounded-l-none md:rounded-r-3xl p-5 lg:p-8 space-y-3 md:space-y-5 lg:space-y-8 xl:space-y-10 flex flex-col justify-center"
           initial={{ opacity: 0, x: -100 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.6 }}
@@ -26,11 +27,13 @@ const ProductsHero = () => {
             A Series
           </h1>
           <p className="text-sm md:text-[16px] md:leading-6">
-            Advanced Robotics System was established by a team of passionate
-            robot enthusiasts dedicated to leveraging cutting-edge technologies
-            to create machines and robots that meet client&apos;s needs in
-            various industrial sectors. The startup was founded in 2020,
-            focusing on industrial robot development and production.
+            The A-Series is a compact Industrial Mobile Robot. Its compact
+            design allows it to navigate through narrow paths and makes it very
+            versatile. Your seamless material handling solution. Say goodbye to
+            complex setups and modifications-with our AMR, there&apos;s no need
+            to make any changes to your factory floor. Our innovative robot
+            adapts to your existing infrastructure, effortlessly handling
+            pallets while you focus on enhancing productivity.
           </p>
           <div className="flex gap-4 items-center group">
             <Button
--- src/sections/products/key-features.tsx
+++ src/sections/products/key-features.tsx
@@ -7,10 +7,10 @@ const KeyFeatures = () => {
       <h2 className="text-2xl md:text-3xl lg:text-4xl font-medium text-midnight-blue mb-5">
         Key Features
       </h2>
-      <p className="text-sm leading-normal md:text-base md:leading-normal w-full lg:w-2/3 xl:w-1/2">
+      {/* <p className="text-sm leading-normal md:text-base md:leading-normal w-full lg:w-2/3 xl:w-1/2">
         The A-Series is a compact Industrial Mobile Robot. Its compact design
         allows it to navigate through narrow paths and makes it very versatile.
-      </p>
+      </p> */}
       <div className="flex flex-wrap gap-12 md:gap-16 lg:gap-20 xl:gap-24 items-end justify-center mt-16">
         {featuresData.map((feature, index) => (
           <FeaturesCard key={index} img={feature.img} title={feature.title} />
