d3c5dfc69fe3e42d1cc921f6e6d2bfa6ff835950
A1-mamun <1154almamun@gmail.com>
2025-01-19 20:11:32 +0600

    Dropdown added in small devices navbar and warehouse section padding added

--- src/sections/header/header.tsx
+++ src/sections/header/header.tsx
@@ -3,18 +3,18 @@ import Link from "next/link";
 import Logo from "@/assets/images/ARS_2_cropped.png";
 
 import {
-	Navbar,
-	NavbarContent,
-	NavbarMenu,
-	NavbarMenuItem,
-	NavbarMenuToggle,
-	Button,
-	NavbarBrand,
-	Dropdown,
-	DropdownTrigger,
-	DropdownMenu,
-	DropdownItem,
-	DropdownSection,
+  Navbar,
+  NavbarContent,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
+  Button,
+  NavbarBrand,
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  DropdownSection,
 } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import { navLinks } from "@/data";
@@ -22,197 +22,264 @@ import Image from "next/image";
 import { ICONS } from "@/utils/icons";
 
 const Header = () => {
-	const [isScrolled, setIsScrolled] = useState(false);
-	const [isExpanded, setIsExpanded] = useState(false);
-	const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-	useEffect(() => {
-		if (typeof window === "undefined") return;
+  useEffect(() => {
+    if (typeof window === "undefined") return;
 
-		const handleScroll = () => {
-			if (window.scrollY > 20) {
-				setIsScrolled(true);
-				setIsExpanded(false);
-			} else {
-				setIsScrolled(false);
-			}
-		};
+    const handleScroll = () => {
+      if (window.scrollY > 20) {
+        setIsScrolled(true);
+        setIsExpanded(false);
+      } else {
+        setIsScrolled(false);
+      }
+    };
 
-		handleScroll();
+    handleScroll();
 
-		window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-		return () => {
-			window.removeEventListener("scroll", handleScroll);
-		};
-	}, []);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-	const toggleMenu = () => {
-		setIsExpanded(!isExpanded);
-	};
+  const toggleMenu = () => {
+    setIsExpanded(!isExpanded);
+  };
 
-	return (
-		<>
-			<div className=" hidden lg:block">
-				<div className="fixed flex top-3 items-center h-16 text-white font-medium z-50 w-full lg:px-16 xl:px-24 2xl:px-36 3xl:px-44">
-					<div className="flex justify-between w-full items-center ">
-						<div
-							className={`${
-								isScrolled
-									? "opacity-0 duration-1000"
-									: "opacity-100 duration-1000"
-							}`}
-						>
-							<Link href="/">
-								<Image src={Logo} alt="ARS" className="w-28" />
-							</Link>
-						</div>
-						<div
-							className={`relative ${
-								isExpanded
-									? "bg-red-500 lg:w-[760px] xl:w-[835px] h-12  duration-1000"
-									: `${
-											isScrolled
-												? "bg-red-500 w-20 h-12  duration-1000 "
-												: "bg-red-500 w-0 h-12 duration-1000 "
-									  }`
-							} `}
-						>
-							<div className="flex lg:gap-2 xl:gap-4 absolute right-0 top-1/2 transform -translate-y-1/2">
-								<ul
-									className={`flex lg:gap-8 xl:gap-10 ${
-										isExpanded
-											? "opacity-100 duration-1000"
-											: `${
-													isScrolled
-														? "opacity-0 duration-1000"
-														: "opacity-100 duration-1000"
-											  }`
-									}`}
-								>
-									{navLinks.map((item, index) => (
-										<li key={index}>
-											{item.name === "Get in Touch" ? (
-												<Button
-													radius="full"
-													className="bg-cyan-blue-30 border border-ars-cyan font-medium lg:px-5 xl:px-6 lg:text-lg hover:text-gray-300 duration-200 "
-												>
-													<Link href={item.link}>{item.name}</Link>
-												</Button>
-											) : item.name === "Products" ? (
-												<Dropdown>
-													<DropdownTrigger endContent={ICONS.chevron}>
-														<Button variant="bordered">Products</Button>
-													</DropdownTrigger>
-													<DropdownMenu
-														aria-label="Product Categories"
-														variant="solid"
-														className="!rounded-xl shadow-lg"
-														
-													>
-														{/* Section 1: Industrial Mobile Robot */}
-														<DropdownSection
-															title="Industrial Mobile Robot"
-															className="bg-ars-cyan text-white pt-1"
-															classNames={{
-																heading: "py-2 px-4",
-															}}
-														>
-															<DropdownItem
-																key="a-series"
-																className="bg-ars-gray hover:bg-ars-gray/80 text-white py-2 px-4"
-															>
-																<Link href="/products">A Series</Link>
-															</DropdownItem>
-															<DropdownItem
-																key="b-series"
-																className="bg-ars-gray hover:bg-ars-gray/80 text-white py-2 px-4"
-															>
-																<Link href="/products">B Series (Coming Soon)</Link>
-															</DropdownItem>
-															<DropdownItem
-																key="f-series"
-																className="bg-ars-gray hover:bg-ars-gray/80 text-white py-2 px-4"
-															>
-																F Series (Coming Soon)
-															</DropdownItem>
-														</DropdownSection>
-														{/* Section 2: No Code Software Platform */}
-														<DropdownSection
-															title="No Code Software Platform "
-															className="bg-ars-cyan text-white pt-1"
-															classNames={{
-																heading: "py-2 px-4",
-															}}
-														>
-															<DropdownItem
-																key="arsfm"
-																className="bg-ars-gray hover:bg-ars-gray/80 text-white py-2 px-4"
-															>
-																<Link href="/products/arsFM">arsFM</Link>
-															</DropdownItem>
-															<DropdownItem
-																key="arsos"
-																className="bg-ars-gray hover:bg-ars-gray/80 text-white py-2 px-4"
-															>
-																arsOs (Coming Soon)
-															</DropdownItem>
-														</DropdownSection>
-													</DropdownMenu>
-												</Dropdown>
-											) : (
-												<Button className="bg-transparent lg:px-1 xl:px-2 lg:text-lg font-medium hover:text-gray-300 duration-200 ">
-													<Link href={item.link}>{item.name}</Link>
-												</Button>
-											)}
-										</li>
-									))}
-								</ul>
+  return (
+    <>
+      <div className=" hidden lg:block">
+        <div className="fixed flex top-3 items-center h-16 text-white font-medium z-50 w-full lg:px-16 xl:px-24 2xl:px-36 3xl:px-44">
+          <div className="flex justify-between w-full items-center ">
+            <div
+              className={`${
+                isScrolled
+                  ? "opacity-0 duration-1000"
+                  : "opacity-100 duration-1000"
+              }`}
+            >
+              <Link href="/">
+                <Image src={Logo} alt="ARS" className="w-28" />
+              </Link>
+            </div>
+            <div
+              className={`relative ${
+                isExpanded
+                  ? "bg-red-500 lg:w-[760px] xl:w-[840px] h-12  duration-1000"
+                  : `${
+                      isScrolled
+                        ? "bg-red-500 w-20 h-12  duration-1000 "
+                        : "bg-red-500 w-0 h-12 duration-1000 "
+                    }`
+              } `}
+            >
+              <div className="flex lg:gap-2 xl:gap-4 absolute right-0 top-1/2 transform -translate-y-1/2">
+                <ul
+                  className={`flex lg:gap-8 xl:gap-10 ${
+                    isExpanded
+                      ? "opacity-100 duration-1000"
+                      : `${
+                          isScrolled
+                            ? "opacity-0 duration-1000"
+                            : "opacity-100 duration-1000"
+                        }`
+                  }`}
+                >
+                  {navLinks.map((item, index) => (
+                    <li key={index}>
+                      {item.name === "Get in Touch" ? (
+                        <Button
+                          radius="full"
+                          className="bg-cyan-blue-30 border border-ars-cyan font-medium lg:px-5 xl:px-6 lg:text-lg hover:text-gray-300 duration-200 "
+                        >
+                          <Link href={item.link}>{item.name}</Link>
+                        </Button>
+                      ) : item.name === "Products" ? (
+                        <Dropdown>
+                          <DropdownTrigger endContent={ICONS.chevron}>
+                            <Button className="pr-0" variant="bordered">
+                              Products
+                            </Button>
+                          </DropdownTrigger>
+                          <DropdownMenu
+                            aria-label="Product Categories"
+                            variant="solid"
+                            className="!rounded-xl shadow-lg"
+                          >
+                            {/* Section 1: Industrial Mobile Robot */}
+                            <DropdownSection
+                              title="Industrial Mobile Robot"
+                              className="bg-ars-cyan text-white pt-1"
+                              classNames={{
+                                heading: "py-2 px-4",
+                              }}
+                            >
+                              <DropdownItem
+                                key="a-series"
+                                className="bg-ars-gray hover:bg-ars-gray/80 text-white py-2 px-4"
+                              >
+                                <Link href="/products">A Series</Link>
+                              </DropdownItem>
+                              <DropdownItem
+                                key="b-series"
+                                className="bg-ars-gray hover:bg-ars-gray/80 text-white py-2 px-4"
+                              >
+                                <Link href="/products">
+                                  B Series (Coming Soon)
+                                </Link>
+                              </DropdownItem>
+                              <DropdownItem
+                                key="f-series"
+                                className="bg-ars-gray hover:bg-ars-gray/80 text-white py-2 px-4"
+                              >
+                                F Series (Coming Soon)
+                              </DropdownItem>
+                            </DropdownSection>
+                            {/* Section 2: No Code Software Platform */}
+                            <DropdownSection
+                              title="No Code Software Platform "
+                              className="bg-ars-cyan text-white pt-1"
+                              classNames={{
+                                heading: "py-2 px-4",
+                              }}
+                            >
+                              <DropdownItem
+                                key="arsfm"
+                                className="bg-ars-gray hover:bg-ars-gray/80 text-white py-2 px-4"
+                              >
+                                <Link href="/products/arsFM">arsFM</Link>
+                              </DropdownItem>
+                              <DropdownItem
+                                key="arsos"
+                                className="bg-ars-gray hover:bg-ars-gray/80 text-white py-2 px-4"
+                              >
+                                arsOs (Coming Soon)
+                              </DropdownItem>
+                            </DropdownSection>
+                          </DropdownMenu>
+                        </Dropdown>
+                      ) : (
+                        <Button className="bg-transparent lg:px-1 xl:px-2 lg:text-lg font-medium hover:text-gray-300 duration-200 ">
+                          <Link href={item.link}>{item.name}</Link>
+                        </Button>
+                      )}
+                    </li>
+                  ))}
+                </ul>
 
-								<Button
-									onClick={toggleMenu}
-									size="sm"
-									className="bg-transparent my-auto lg:pr-5 xl:pr-7 lg:text-xl hover:text-gray-300 duration-200  "
-								>
-									{isExpanded ? ICONS.close : ICONS.menu}
-								</Button>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+                <Button
+                  onClick={toggleMenu}
+                  size="sm"
+                  className="bg-transparent my-auto lg:pr-5 xl:pr-7 lg:text-xl hover:text-gray-300 duration-200  "
+                >
+                  {isExpanded ? ICONS.close : ICONS.menu}
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-			<Navbar
-				shouldHideOnScroll
-				height="2rem"
-				onMenuOpenChange={setIsMenuOpen}
-				className="lg:hidden fixed py-2"
-			>
-				<NavbarContent justify="start">
-					<NavbarBrand>
-						<Link href="/">
-							<Image src={Logo} alt="White Code" className="w-16" />
-						</Link>
-					</NavbarBrand>
-				</NavbarContent>
-				<NavbarContent justify="end">
-					<NavbarMenuToggle
-						aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-					/>
-				</NavbarContent>
+      <Navbar
+        shouldHideOnScroll
+        height="2rem"
+        onMenuOpenChange={setIsMenuOpen}
+        className="lg:hidden fixed py-2"
+      >
+        <NavbarContent justify="start">
+          <NavbarBrand>
+            <Link href="/">
+              <Image src={Logo} alt="ARS" className="w-16" />
+            </Link>
+          </NavbarBrand>
+        </NavbarContent>
+        <NavbarContent justify="end">
+          <NavbarMenuToggle
+            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          />
+        </NavbarContent>
 
-				<NavbarMenu className="z-50 mt-4">
-					{navLinks.map((item, index) => (
-						<NavbarMenuItem key={`${item}-${index}`}>
-							<Link className="w-full text-white" href={item.link}>
-								{item.name}
-							</Link>
-						</NavbarMenuItem>
-					))}
-				</NavbarMenu>
-			</Navbar>
-		</>
-	);
+        <NavbarMenu className="z-50 mt-4">
+          {navLinks.map((item, index) => (
+            <NavbarMenuItem key={`${item}-${index}`}>
+              <Link className="w-full text-white" href={item.link}>
+                {item.name === "Products" ? (
+                  <Dropdown>
+                    <DropdownTrigger endContent={ICONS.chevron}>
+                      <Button className="pl-0" variant="bordered">
+                        Products
+                      </Button>
+                    </DropdownTrigger>
+                    <DropdownMenu
+                      aria-label="Product Categories"
+                      variant="solid"
+                      className="!rounded-xl shadow-lg"
+                    >
+                      {/* Section 1: Industrial Mobile Robot */}
+                      <DropdownSection
+                        title="Industrial Mobile Robot"
+                        className="bg-ars-cyan text-white pt-1"
+                        classNames={{
+                          heading: "py-2 px-4",
+                        }}
+                      >
+                        <DropdownItem
+                          key="a-series"
+                          className="bg-ars-gray hover:bg-ars-gray/80 text-white py-2 px-4"
+                        >
+                          <Link href="/products">A Series</Link>
+                        </DropdownItem>
+                        <DropdownItem
+                          key="b-series"
+                          className="bg-ars-gray hover:bg-ars-gray/80 text-white py-2 px-4"
+                        >
+                          <Link href="/products">B Series (Coming Soon)</Link>
+                        </DropdownItem>
+                        <DropdownItem
+                          key="f-series"
+                          className="bg-ars-gray hover:bg-ars-gray/80 text-white py-2 px-4"
+                        >
+                          F Series (Coming Soon)
+                        </DropdownItem>
+                      </DropdownSection>
+                      {/* Section 2: No Code Software Platform */}
+                      <DropdownSection
+                        title="No Code Software Platform "
+                        className="bg-ars-cyan text-white pt-1"
+                        classNames={{
+                          heading: "py-2 px-4",
+                        }}
+                      >
+                        <DropdownItem
+                          key="arsfm"
+                          className="bg-ars-gray hover:bg-ars-gray/80 text-white py-2 px-4"
+                        >
+                          <Link href="/products/arsFM">arsFM</Link>
+                        </DropdownItem>
+                        <DropdownItem
+                          key="arsos"
+                          className="bg-ars-gray hover:bg-ars-gray/80 text-white py-2 px-4"
+                        >
+                          arsOs (Coming Soon)
+                        </DropdownItem>
+                      </DropdownSection>
+                    </DropdownMenu>
+                  </Dropdown>
+                ) : (
+                  item.name
+                )}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </NavbarMenu>
+      </Navbar>
+    </>
+  );
 };
 
 export default Header;
--- src/sections/home/warehouse-ref.tsx
+++ src/sections/home/warehouse-ref.tsx
@@ -3,21 +3,21 @@ import React from "react";
 const WarehouseRef = () => {
   return (
     <div
-      className="bg-cover bg-center w-full min-h-screen lg:min-h-[135vh] flex items-start px-6 lg:px-24 py-12 md:py-36"
+      className="bg-cover bg-center w-full min-h-screen lg:min-h-[135vh] flex items-start padding-responsive"
       style={{ backgroundImage: "url('/warehouse-ref.jpg')" }}
     >
       {/* Content Container */}
       <div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-96 mx-auto text-white">
         {/* Left Column: Title */}
         <div className="flex items-center">
-          <h1 className="text-3xl lg:text-5xl font-bold">
+          <h1 className="text-2xl md:text-4xl lg:text-5xl xl:text-6xl ">
             Take it to the next level on-site.
           </h1>
         </div>
 
         {/* Right Column: Description */}
         <div className="flex items-center">
-          <p className="text-sm lg:text-lg leading-relaxed text-justify">
+          <p className="text-xs leading-normal md:text-sm md:leading-normal lg:text-base lg:leading-normal xl:text-lg xl:leading-normal">
             A robot alone is just a tool, but when fully integrated into your
             operations, it becomes a transformative solution. Imagine seamless
             autonomous material flows powering your efficiency, smart Fleet
@@ -26,7 +26,7 @@ const WarehouseRef = () => {
             advanced robotics and intelligent systems, your intralogistics can
             go beyond limits—unlocking new levels of productivity and
             innovation.
-						<br/ >
+            <br />
             The sky isn&apos;t the limit; it&apos;s just the beginning.
           </p>
         </div>
