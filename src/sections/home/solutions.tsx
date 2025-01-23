0b7e650eaa6e86a955fa17fc9f36515d2a1ad690
A1-mamun <1154almamun@gmail.com>
2025-01-23 18:32:51 +0600

    Home page solution section small device view update

--- src/sections/home/solutions.tsx
+++ src/sections/home/solutions.tsx
@@ -14,14 +14,16 @@ const Solutions = () => {
           reliability.
         </p>
       </header>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  gap-10 md:gap-16 xl:gap-20">
+      <div className="grid grid-cols-2 lg:grid-cols-4  gap-7 md:gap-16 xl:gap-20">
         {solutionsData.map((solution, index) => (
-          <article key={index} className="w-4/5 md:w-full">
-            <h1 className="text-5xl font-bold text-ars-cyan">
+          <article key={index} className="w-full">
+            <h1 className="text-3xl md:text-5xl text-ars-cyan">
               {solution.icon}
             </h1>
-            <h2 className="text-xl font-bold mt-5">{solution.title}</h2>
-            <p className="text-lg mt-3 leading-normal">
+            <h2 className="text-lg md:text-xl font-semibold md:font-bold mt-3 md:mt-5">
+              {solution.title}
+            </h2>
+            <p className="text-sm md:text-lg mt-2 md:mt-3 leading-normal">
               {solution.description}
             </p>
           </article>
