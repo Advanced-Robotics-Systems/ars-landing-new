a64e8172ae601ac7784e127a781ea407c7d42ce7
Abrar Hussain Marzuk <abrar.16@protonmail.com>
2025-01-29 14:00:13 +0600

    Added separate bg image and warehouse image in warehouse-ref section

--- src/sections/home/warehouse-ref.tsx
+++ src/sections/home/warehouse-ref.tsx
@@ -1,15 +1,21 @@
+import Image from "next/image";
+
 const WarehouseRef = () => {
   return (
     <div
-      className="w-full min-h-screen lg:min-h-[135vh] flex items-start padding-responsive"
+      className="relative isolate w-full min-h-screen lg:min-h-[135vh] flex items-start padding-responsive"
       style={{
-        backgroundImage: "url('/warehouse-ref.jpg')",
-        backgroundSize: "contain",
+        backgroundImage: "url('/warehouse-black-background.jpg')",
+        backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
-        backgroundColor: "black",
       }}
     >
+      <Image
+        src="/warehouse-no-text-no-bg.png"
+        alt="Warehouse Picture"
+        className="absolute top-0 left-0 w-full h-full object-contain object-[center_90%] xxs:object-[center_80%] xs:object-center -z-10"
+      />
       {/* Content Container */}
       <div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-5 md:gap-8 lg:gap-20 xl:gap-24 2xl:gap-36 3xl:gap-40 mx-auto text-white">
         {/* Left Column: Title */}
