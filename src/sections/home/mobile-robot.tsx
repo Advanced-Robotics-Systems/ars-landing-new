83a18c0b7350cee6e439fca1df14b7bc3d871119
Abrar Hussain Marzuk <142195422+abrar-16m@users.noreply.github.com>
2025-02-04 00:00:42 +0600

    Bug fixed in mobile-robot section in home

--- src/sections/home/mobile-robot.tsx
+++ src/sections/home/mobile-robot.tsx
@@ -30,7 +30,11 @@ const MobileRobot = () => {
         <div
           className="hidden md:block md:h-[65vh] lg:h-[75vh] w-full rounded-t-3xl"
           style={{
-            background: `linear-gradient(270deg, rgba(0, 0, 0, 0.00) 40%, #000 100%),url(${currentData.img}) white 100% / contain no-repeat`,
+            background: `linear-gradient(270deg, rgba(0, 0, 0, 0.00) 40%, #000 100%), url(${currentData.img})`,
+            backgroundColor: "white",
+            backgroundSize: "contain",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
           }}
         >
           <div className="h-full rounded-t-3xl text-white flex flex-col justify-end w-56 md:w-80 lg:w-[450px] space-y-2 md:space-y-5 px-5 py-3 md:px-12 md:py-8 lg:px-16 lg:py-10 xl:px-20 xl:py-12">
