ce23ca46fb547be92842a7fd99feb7026e849af8
abrar-16m <abrar.16@protonmail.com>
2025-03-12 16:14:07 +0300

    Added translations for hero page in home

--- src/sections/home/hero.tsx
+++ src/sections/home/hero.tsx
@@ -2,14 +2,17 @@
 
 import { Button } from "@nextui-org/react";
 import { Link } from "@/i18n/navigation";
+import { useTranslations } from "next-intl";
 
 const Hero = () => {
+  const t = useTranslations("homepage.hero");
+
   return (
     <section className="min-h-[300px] h-screen w-full bg-[url('/home-hero.jpeg')] bg-cover bg-[85%_center]">
       <div className="bg-gradient-to-tl from-[#FFFFFF00] to-[#000000B3] h-full padding-responsive flex flex-col justify-center gap-2 md:gap-3 text-ars-primary">
         {/* <p className="text-xs lg:text-sm">An ARS Robot Development</p> */}
         <h2 className="text-xl md:text-4xl lg:text-5xl xl:text-6xl font-medium">
-          Revolutionising Automation <br /> Driving Innovation
+          {t("headingTop")} <br /> {t("headingBottom")}
         </h2>
         <div>
           <Button
@@ -19,7 +22,7 @@ const Hero = () => {
             radius="full"
             className="bg-cyan-blue px-6 md:px-12 py-4 md:py-6 text-sm md:text-base font-medium text-ars-gray mt-2"
           >
-            Our Company
+            {t("buttonText")}
           </Button>
         </div>
       </div>
