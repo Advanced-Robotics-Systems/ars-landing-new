994cf111ead9af1afa8aae1efa84b59f900fcab4
Tahzeeb <ishmum20000@gmail.com>
2025-05-01 13:09:27 +0200

    Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,28 +5,30 @@ import { Footer } from "@/sections";
 import NextUIWrapper from "./nextui-wrapper";
 
 const montserrat = Montserrat({
-  subsets: ["latin"],
-  fallback: ["Arial", "sans-serif"],
+	subsets: ["latin"],
+	fallback: ["Arial", "sans-serif"],
 });
 
 export const metadata: Metadata = {
-  title: "Advanced Robotics Systems",
-  description: "Revolutionising Automation, Driving Innovation",
+	title:
+		"Advanced Robotics Systems (ARS) | Revolutionizing Automation & Robotics",
+	description:
+		"Advanced Robotics Systems (ARS) is a leading robotics and automation company based in Riyadh, Saudi Arabia. We specialize in cutting-edge autonomous systems, warehouse robotics, and industrial automation tailored to modern industries.",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={montserrat.className}>
-        <NextUIWrapper>
-          {children}
-          <Footer />
-        </NextUIWrapper>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={montserrat.className}>
+				<NextUIWrapper>
+					{children}
+					<Footer />
+				</NextUIWrapper>
+			</body>
+		</html>
+	);
 }
