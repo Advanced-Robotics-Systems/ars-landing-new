db9e5a14c788d63cb058fdd8830fd5080d4bca6a
abrar-16m <abrar.16@protonmail.com>
2025-05-07 10:26:51 +0300

    from and to changed for sending emails

--- src/app/api/submit-form/route.ts
+++ src/app/api/submit-form/route.ts
@@ -1,31 +1,3 @@
-// import { NextResponse } from "next/server";
-
-// export async function POST(request: Request) {
-//   const formData = await request.json();
-
-//   try {
-//     const scriptUrl = process.env.WEB_APP_URL as string;
-//     console.log(formData);
-
-//     const res = await fetch(scriptUrl, {
-//       method: "POST",
-//       body: new URLSearchParams(formData),
-//     });
-
-//     if (!res.ok) {
-//       throw new Error("Failed to submit form data");
-//     }
-
-//     return NextResponse.json({ message: "Form submitted successfully!" });
-//   } catch (error) {
-//     console.error(error);
-//     return NextResponse.json(
-//       { message: "Error submitting form." },
-//       { status: 500 }
-//     );
-//   }
-// }
-
 import { EmailTemplate } from "@/components";
 import { ReactElement } from "react";
 import { Resend } from "resend";
@@ -37,8 +9,8 @@ export async function POST(request: Request) {
 
   try {
     const { data, error } = await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
-      to: ["abrarhussain@arsystems.org"],
+      from: "ARS Website <abrarhussain@arsystems.org>",
+      to: ["inquiries@arsystems.org"],
       subject: "Form Submission (ARS)",
       react: EmailTemplate(formData) as ReactElement,
     });
